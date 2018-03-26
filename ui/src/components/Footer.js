0320a695b2d18ea9b238196a67e09d926f45564a
Masooma Malik <36923471+mmalik0619@users.noreply.github.com>
2018-03-26 23:21:12 +0100

    updated footer

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -1,28 +1,65 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import Radium from 'radium';
+
 const style = {
 
   body: {
     position: 'relative',
     bottom: '0px',
-    height: '60px',
+    height: '120px',
     width: '100%',
-    backgroundColor: 'black'
+    backgroundColor: '#000000'
   },
 
   footer: {
-    color: 'white',
-    float: 'right',
-    marginRight: '250px'
+    marginLeft: '500px',
+    paddingTop: '40px'
+  },
+
+  list: {
+    display: 'inline',
+    color: '#0b9d00',
+    fontSize: '17px'
+  },
+
+  listElement: {
+    color: '#ffffff'
+  },
+
+  copyright: {
+    color: '#b3b3b3',
+    fontSize: '11px',
+    display: 'inline-block',
+    margin: '0 0 0 50px'
+  },
+
+  logo: {
+    float: 'left',
+    color: '#ffffff',
+    fontSize: '25px'
+  }
+
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div style={style.body}>
+        <div style={style.footer}>
+          <ul>
+            <li key="1" style={style.list}><Link to={this.props.element1Link} style={style.listElement}>{this.props.element1}</Link> | </li>
+            <li key="2" style={style.list}><Link to={this.props.element2Link} style={style.listElement}>{this.props.element2}</Link> | </li>
+            <li key="3" style={style.list}><Link to={this.props.element3Link} style={style.listElement}>{this.props.element3}</Link></li>
+          </ul>
+          <div style={style.logo}>EduEvents</div>
+          <div style={style.copyright}>Copyright 2018</div>
+        </div>
+
+      </div>
+    );
   }
 }
 
-const Footer = () => {
-  return (
-    <div style={style.body}>
-      <h6 style={style.footer}>Footer</h6>
-    </div>
-  );
-}
-
-export default Footer;
+export default Radium(Footer);
