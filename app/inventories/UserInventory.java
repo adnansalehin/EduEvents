e76761230317c7da62c54299d7f976246b1966c7
Adnan Salehin <adnansalehin03@gmail.com>
2018-03-26 01:30:17 +0100

    SQLite db added

--- app/inventories/UserInventory.java
+++ app/inventories/UserInventory.java
@@ -2,6 +2,10 @@ package inventories;
 
 import lombok.Data;
 import models.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -13,6 +17,29 @@ public class UserInventory {
 		users.add(new User("adnan2", "qwerty", "adnan@qwe.com"));
     }
     public boolean isRegistered(User user) {
-        return (users.contains(user))
+        return (users.contains(user));
     }
+
+    public UserInventory() {
+		Connection conn = null;
+	    try {
+		    // db parameters
+		    String url = "jdbc:sqlite:C:/Users/adnan/EduEvents/app/database\\eduevents.db";
+		    // create a connection to the database
+		    conn = DriverManager.getConnection(url);
+
+		    System.out.println("Connection to SQLite has been established.");
+
+	    } catch (SQLException e) {
+		    System.out.println(e.getMessage());
+	    } finally {
+		    try {
+			    if (conn != null) {
+				    conn.close();
+			    }
+		    } catch (SQLException ex) {
+			    System.out.println(ex.getMessage());
+		    }
+	    }
+	}
 }
